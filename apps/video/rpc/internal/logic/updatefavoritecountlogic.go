600b454d2be4bda7ef6a6e3b09e0733e82840636
YiZou89 <zouyi89@outlook.com>
2023-10-11 18:58:43 +0800

    feat(video): 添加更新计数功能

--- apps/video/rpc/internal/logic/updatefavoritecountlogic.go
+++ apps/video/rpc/internal/logic/updatefavoritecountlogic.go
@@ -2,6 +2,7 @@ package logic
 
 import (
 	"context"
+	"strconv"
 
 	"github.com/YiZou89/zero-tiktok/apps/video/rpc/internal/svc"
 	"github.com/YiZou89/zero-tiktok/apps/video/rpc/model"
@@ -26,5 +27,22 @@ func NewUpdateFavoriteCountLogic(ctx context.Context, svcCtx *svc.ServiceContext
 func (l *UpdateFavoriteCountLogic) UpdateFavoriteCount(in *model.UpdateFavoriteCountRequest) (*model.UpdateFavoriteCountResponse, error) {
 	// todo: add your logic here and delete this line
 
+	var sqlStr1 string
+	var err error
+	if in.ActionType {
+		sqlStr1 = `update tiktok_video.video_count set favorite_count = favorite_count + 1 where video_id = ? limit 1`
+	} else {
+		sqlStr1 = `update tiktok_video.video_count set favorite_count = favorite_count - 1 where video_id = ? limit 1`
+	}
+	if _, err = l.svcCtx.VideoDB.Exec(sqlStr1, in.VideoId); err != nil {
+		logx.Error("db update favorite count ", err)
+		return nil, err
+	}
+
+	// delete cache
+	vidStr := strconv.FormatInt(in.VideoId, 10)
+	if _, err = l.svcCtx.VideoCache.Del(context.Background(), model.VideoCountPrefix+vidStr).Result(); err != nil {
+		return nil, err
+	}
 	return &model.UpdateFavoriteCountResponse{}, nil
 }
--- apps/video/rpc/model/newmodel.go
+++ apps/video/rpc/model/newmodel.go
@@ -1,52 +1,8 @@
 package model
 
-import (
-	"database/sql"
-	"github.com/zeromicro/go-zero/core/logx"
-	"strconv"
-)
-
 const (
 	VideoInfoPrefix    = "tiktok:video:info:"    // +vid, hash	(info)
 	VideoCountPrefix   = "tiktok:video:count:"   // +vid, hash	(count)
 	VideoFeedPrefix    = "tiktok:video:feed::"   // +nil zset	(vid, timestamp)
 	VideoPublishPrefix = "tiktok:video:publish:" // +uid set (vid)
 )
-
-func (r *Repo) UpdateFavorTx(userId, authorId int64, actionType bool) (err error) {
-	// 更新数据库， 然后删除缓存
-	var sqlStr1, sqlStr2 string
-	if actionType {
-		sqlStr1 = `update tiktok_user.user_count set favorite_count = favorite_count + 1 where user_id = ? limit 1`
-		sqlStr2 = `update tiktok_user.user_count set total_favorited = total_favorited + 1 where user_id = ? limit 1`
-	} else {
-		sqlStr1 = `update tiktok_user.user_count set favorite_count = favorite_count - 1 where user_id = ? limit 1`
-		sqlStr2 = `update tiktok_user.user_count set total_favorited = total_favorited - 1 where user_id = ? limit 1`
-	}
-	tx, err := r.db.BeginTx(context.Background(), &sql.TxOptions{})
-	if err != nil {
-		logx.Error("db transaction begin ", err)
-		return err
-	}
-	defer tx.Rollback()
-	if _, err = tx.Exec(sqlStr1, userId); err != nil {
-		return err
-	}
-	if _, err = tx.Exec(sqlStr2, authorId); err != nil {
-		return err
-	}
-	err = tx.Commit()
-	if err != nil {
-		logx.Error("db transaction commit ", err)
-		return err
-	}
-
-	// delete cache
-	uidStr := strconv.FormatInt(userId, 10)
-	aidStr := strconv.FormatInt(authorId, 10)
-	pipeline := r.rdb.Pipeline()
-	r.rdb.Del(context.Background(), UserCountPrefix+uidStr)
-	r.rdb.Del(context.Background(), UserCountPrefix+aidStr)
-	_, err = pipeline.Exec(context.Background())
-	return err
-}
