07e263931c3a0ac63cc8c67a30acef440e72f9fd
YiZou89 <zouyi89@outlook.com>
2023-11-09 16:56:02 +0800

    修改配置

---
+++ init.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE if not exists tiktok_user;
+CREATE DATABASE if not exists tiktok_video;
+CREATE DATABASE if not exists tiktok_comment;
+CREATE DATABASE if not exists tiktok_favorite;
+CREATE DATABASE if not exists tiktok_follow;
+CREATE DATABASE if not exists tiktok_message;
